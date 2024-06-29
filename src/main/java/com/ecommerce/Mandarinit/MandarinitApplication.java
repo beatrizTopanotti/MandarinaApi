142d556fbf7126733c0e140047456460990bf6c2
beatrizTopanotti <beatriz.topanotti.ff@gmail.com>
2024-06-29 14:28:33 -0300

    criadas variáveis de ambiente

--- src/main/java/com/ecommerce/Mandarinit/MandarinitApplication.java
+++ src/main/java/com/ecommerce/Mandarinit/MandarinitApplication.java
@@ -1,13 +1,14 @@
 package com.ecommerce.Mandarinit;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class MandarinitApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MandarinitApplication.class, args);
 	}
 
